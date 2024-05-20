fd6fd4087c0f1a428e87900203c30143028a9d63
cabrel19 <cabrelnya0@gmail.com>
2024-05-20 13:39:18 +0100

    add image

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -11,7 +11,7 @@ export default function HomeScreen() {
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
       headerImage={
         <Image
-          source={require('@/assets/images/partial-react-logo.png')}
+          source={require('@/assets/images/fond.png')}
           style={styles.reactLogo}
         />
       }>
@@ -62,7 +62,7 @@ const styles = StyleSheet.create({
   },
   reactLogo: {
     height: 178,
-    width: 290,
+    width: 425,
     bottom: 0,
     left: 0,
     position: 'absolute',
